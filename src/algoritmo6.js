103fa805184c4e511af58e207137feb89d38f18a
Isaac Yuri <isaacyuri22@gmail.com>
2024-11-06 15:00:37 -0300

    :sparkles: feat: segundo commit

--- src/algoritmo6.js
+++ src/algoritmo6.js
@@ -4,7 +4,7 @@ const input = prompt("Digite um numero"); // mensagem colocada no prompt
 // pega o numero que o usuario digitar e convente para inteiro
 const inputValue = parseInt(input); // o nome esta parserInt mas a funcao que converte uma string para inteiro é parseInt
 
-// declara um array com o tamanho no numero  
+// declara um array com tamanho igual ao numero que o usuario digitar e preenche esses espaços com null   
 const array = new Array(inputValue).fill(null);
 
 let values = new Array(input)
